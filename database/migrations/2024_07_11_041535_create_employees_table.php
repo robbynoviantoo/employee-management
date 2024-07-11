53ae28ef846426d206a5661b1f59be2002ce7ed3
robbynoviantoo <heyitsrobby99@gmail.com>
2024-07-11 11:22:44 +0700

    Employee-Management

---
+++ database/migrations/2024_07_11_041535_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nama');
+            $table->string('Foto');
+            $table->string('Jabatan');
+            $table->string('Gedung');
+            $table->string('Area');
+            $table->string('Cell');
+            $table->integer('HP');
+            $table->date('Date In');
+            $table->string('Status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
